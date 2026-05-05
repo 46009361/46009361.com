56f620ded03e6651ece88a12c899e1270b5c361a
46009361 <me@46009361.com>
2026-05-04 17:11:53 -0700

    Add link to 'Type a Random Word' in navigation

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<nav><a href="flying-game">Flying Game with Save and Load!</a> | <a href="chemistry-flashcards">Chemistry Flashcards</a> | you never know what's next…</nav>
+<nav><a href="flying-game">Flying Game with Save and Load!</a> | <a href="chemistry-flashcards">Chemistry Flashcards</a> | <a href="https://46009361.online">Type a Random Word</a> | you never know what's next…</nav>
