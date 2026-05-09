f7d462f44d851984a10f80af3c1656e285fd3f24
46009361 <29450578-46009361@users.noreply.gitlab.com>
2026-05-09 16:30:31 -0700

    refactor: move from AWS to static site generator

--- index.php
+++
@@ -1,2 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<nav><a href="flying-game">Flying Game with Save and Load!</a> | <a href="chemistry-flashcards">Chemistry Flashcards</a> | <a href="https://46009361.online">Type a Random Word</a> | you never know what's next…</nav>
--- scripts/before_block_traffic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# BeforeBlockTraffic hook - runs before traffic is blocked
-echo "Preparing to block traffic from load balancer"
-exit 0
